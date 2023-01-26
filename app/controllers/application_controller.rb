1b77db2419d3f8e3c5f892c98ad1f9604a1f7cc4
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-01-26 16:13:56 +0000

    All pages working with random generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,25 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template=>"game_templates/rules.html.erb"})
+  end
+  
   def play_rock
     
     #redirect_to("https://wikipedia.org")
     render({ :template=>"game_templates/user_rock.html.erb"})
   end
+
+  def play_paper
+
+    render({ :template=>"game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+
+    render({ :template=>"game_templates/user_scissors.html.erb"})
+  end
+
+  
 end
