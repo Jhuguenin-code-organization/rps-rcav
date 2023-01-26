f42de7dfd4df1cc1af38231bb06aed147aca567d
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-01-26 16:59:31 +0000

    All correct but with instance variables and layout settings for "Play X" links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -9,18 +9,41 @@ class ApplicationController < ActionController::Base
   end
   
   def play_rock
-    
-    #redirect_to("https://wikipedia.org")
+    @comp_move= ["rock", "paper", "scissors"].sample
+
+    if @comp_move=="rock" 
+    @outcome= "tied"
+   elsif @comp_move=="paper"
+    @outcome= "lost"
+    elsif @comp_move=="scissors" 
+    @outcome= "won"
+    end 
     render({ :template=>"game_templates/user_rock.html.erb"})
   end
 
   def play_paper
+    @comp_move= ["rock", "paper", "scissors"].sample
 
+    if @comp_move=="rock" 
+    @outcome= "won"
+   elsif @comp_move=="paper"
+    @outcome= "tied"
+    elsif @comp_move=="scissors" 
+    @outcome= "lost"
+    end 
     render({ :template=>"game_templates/user_paper.html.erb"})
   end
 
   def play_scissors
+    @comp_move= ["rock", "paper", "scissors"].sample
 
+    if @comp_move=="rock" 
+    @outcome= "lost"
+   elsif @comp_move=="paper"
+    @outcome= "won"
+    elsif @comp_move=="scissors" 
+    @outcome= "tied"
+    end 
     render({ :template=>"game_templates/user_scissors.html.erb"})
   end
 
